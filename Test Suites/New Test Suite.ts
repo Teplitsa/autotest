f683df52464e9bfcfa789505272036b9a3248618
Krapivnitskiy <111494614+Krapivnitskiy@users.noreply.github.com>
2022-08-18 14:09:08 +0300

    Add files via upload

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -10,40 +10,12 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>6483ff56-9836-4a9a-92a6-0efed744e17c</testSuiteGuid>
+   <testSuiteGuid>c6f5183a-0561-48ce-8ab1-89f0041fdf80</testSuiteGuid>
    <testCaseLink>
-      <guid>19982c95-5062-4b51-a5fb-eb324d76d441</guid>
+      <guid>1d50c79e-8c13-4d2c-a225-77223c831158</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/simple payment</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>13bf436e-2e15-4b43-aef9-802b6343008a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Mysumm</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>e0aa40d9-5deb-4d0b-9fbb-750f0b14519a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/customfield</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>651a706c-02f1-4db9-90f9-05a4b3c10f47</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/useotherpayment</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>c2f0f83c-0b4f-48ca-aafb-5af7e3e9998c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/withprize</testCaseId>
+      <testCaseId>Test Cases/checktitles</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
